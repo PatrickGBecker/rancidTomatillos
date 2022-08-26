b1844e0c0d5be3bba4bb0da92aad43f54cc9ae67
Patrick Becker <patrickgbecker@gmail.com>
2022-08-25 19:33:28 -0500

    Added props iterations

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import Header from "./Header"
+import MovieView from "./MovieView"
+ //import MovieCard from "./MovieCard"
+import getData from "./apiCalls"
+import movieData from "./sample-data"
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super() 
+    this.state = {
+      movies: []
+    }
+  }
+
+  componentDidMount = () => {
+    this.setState({movies: movieData.movies})
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <MovieView 
+          movieData={this.state.movies}/>
+        {/* <SingleMovie />  */}
+      </div>
+    )
+  }
 }
-
 export default App;
